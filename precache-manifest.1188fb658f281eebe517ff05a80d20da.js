f19658f3266cbedc93385322fa8ada8592ffc649
Vitalii Baitaliuk <vitalii.baitaliuk@agiliway.com>
2020-04-01 19:55:56 +0300

    Updates

---
+++ precache-manifest.1188fb658f281eebe517ff05a80d20da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8399ab1debc2486d3760bef0ebc8ade",
+    "url": "/books-kiosk/index.html"
+  },
+  {
+    "revision": "28855f79af6117206f32",
+    "url": "/books-kiosk/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fd0b750f12cf9dde3d44",
+    "url": "/books-kiosk/static/js/2.b4f8ec94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/books-kiosk/static/js/2.b4f8ec94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28855f79af6117206f32",
+    "url": "/books-kiosk/static/js/main.c9132d40.chunk.js"
+  },
+  {
+    "revision": "2a789cc8f95c3d76fffa",
+    "url": "/books-kiosk/static/js/runtime-main.eed4bb2d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/books-kiosk/static/media/logo.5d5d9eef.svg"
+  }
+]);
