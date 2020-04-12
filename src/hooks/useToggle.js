d1fe355dbac55a26ebe168bfeb08820426259ca7
Vitalii Baitaliuk <vitalii.baitaliuk@agiliway.com>
2020-04-12 16:32:24 +0300

    Update useToggle
    - test
    - test commit 2

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -2,7 +2,7 @@ import { useCallback, useState } from 'react';
 
 function useToggle(initialState = false) {
   const [state, setState] = useState(initialState);
-  const toggle = useCallback(() => setState((s) => !s), []);
+  const toggle = useCallback(() => setState((st) => !st), []);
 
   return [state, toggle];
 }
